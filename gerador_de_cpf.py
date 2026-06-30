cdd84748ff8dbcaf47249b30e54cfac49a59551d
mateus002 <cyberjobs19@gmail.com>
2026-06-29 23:28:03 -0300

    atualização. mudando funções

--- gerador_de_cpf.py
+++ gerador_de_cpf.py
@@ -1,31 +1,27 @@
 from random import randint
 from time import sleep
-def numero1():
-    numero = randint(0,9)
-    cpf = [randint(0,9) for _ in range(9)]
-    multiplicadores = [x for x in range(10,1,-1)]
-    novo_cpf = [a * b for a,b in zip(cpf,multiplicadores)]
-    soma = sum(novo_cpf) % 11
-    if soma == 0 or soma == 1:
-        cpf.append(0)
-    else:
-        cpf.append(11-soma)
-    return cpf
 
-def numero2():
-    cpf = numero1()
-    multiplicadores2 = [x for x in range(11,1,-1)]
-    novo_cpf2 = [a * b for a,b in zip(cpf,multiplicadores2)]
-    soma2 = sum(novo_cpf2) % 11
-    if soma2 == 0 or soma2 == 1:
-        cpf.append(0)
-    else:
-        cpf.append(11-soma2)
+def calcular_digito(cpf, multiplicador):
+    
+    soma = sum(a * b for a,b in zip(cpf, multiplicador)) % 11
 
-    return cpf
+    if soma < 2:
+        return 0
+    
+    return 11 - soma
+
+def numeros():
+    return [randint(0,9) for _ in range(9)]
 
 while(True):
-    cpf = "".join(map(str, numero2()))
+    cpff = numeros()
+
+    cpff.append(calcular_digito(cpff, range(10, 1, -1)))
+    cpff.append(calcular_digito(cpff, range(11, 1, -1)))
+
+
+
+    cpf = "".join(map(str, cpff))
     novo_cpf = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
     print(novo_cpf)
     sleep(1.0)
